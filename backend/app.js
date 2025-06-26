8afa30bf01080263fadd96ec6a68fa4fce0871ef
Adarsh kumar <adarshkumar0128@gmail.com>
2025-06-26 07:07:05 +0530

    models created

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const DB = require("./config/db.js");
 require("dotenv").config();
 
 const app = express();
 
-app.listen(process.env.PORT, () => {
-  console.log("App is listening on port 5000");
-});
+DB()
+  .then(() => {
+    console.log("DB connected ");
+    app.listen(process.env.PORT, () => {
+      console.log("App is listening on port 5000");
+    });
+  })
+  .catch((err) => {
+    console.error(err);
+  });
